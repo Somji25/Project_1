3ed063c010032c3767a1141a6e893a25e13f4425
Nawaphon_Luthapak <154293106+Somji25@users.noreply.github.com>
2025-03-18 12:49:33 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import axios from 'axios';
-
 dotenv.config();
 const app = express();
 app.use(express.json());//=ช่วยให้เห็น body 
+import { getConfigsTobit, getConfigsTobitpage, getlogstobit, getstatustobit, postlogstobit } from './pro_handdler.js';
+
 
 
 
@@ -25,86 +25,15 @@ res.send('Welcome to Project Webappication')
 
 
 
-app.get('/configs', async (req,res) =>{
-
-const url = process.env.URL_ENV
-try{
-    const response = await axios.get(url)
-    const data = response.data.data
-    res.send(data)
-}catch(eror){
-    console.log(eror)
-    res.send(eror)
-}
-})
+app.get('/configs', getConfigsTobit)
 
 
-app.get('/configs/:yourDroneId', async (req,res) =>{
-    const droneId = req.params.yourDroneId
-    try{
-        const url = process.env.URL_ENV
-        const response = await axios.get(url)
-        const data = response.data.data
-        const selectitem = data.map(data => {return {drone_id: data.drone_id, drone_name: data.drone_name, light: data.light, country: data.country ,weigh: data.weight}})
-        const finditem = selectitem.find(selectitem => selectitem.drone_id === Number(droneId))
-        res.send(finditem)
-
-    }catch(eror){
-        console.log(eror)
-    }
-})
+app.get('/configs/:yourDroneId', getConfigsTobitpage)
 
 
-app.get('/status/:yourDroneId', async (req,res) =>{
-    const droneId = req.params.yourDroneId
-    try{
-        const url = process.env.URL_ENV
-        const response = await axios.get(url)
-        const data = response.data.data
-        //const selectitem = data.map(data => {return {condition: data.condition}}) */
-        const finditem = data.find(data => data.drone_id === Number(droneId))
-        res.send({condition : finditem.condition})
-
-    }catch(eror){
-        console.log(eror)
-    }
-})
+app.get('/status/:yourDroneId', getstatustobit)
 
 
-app.get('/logs/:yourDroneId', async (req,res) =>{
-    const droneId = req.params.yourDroneId
-    try{
-        const url = process.env.URL_ENV_2
-        const response = await axios.get(url)
-        const data = response.data.items
-        //const selectitem = data.map(data => {return {condition: data.condition}}) */
-        const selectitem = data.map(data => {return {drone_id: data.drone_id, drone_name: data.drone_name, created: data.created, country: data.country ,celsius: data.celsius}})
-        const finditem = selectitem.find(selectitem => selectitem.drone_id === Number(droneId))
-        res.send(finditem)
+app.get('/logs/:yourDroneId', getlogstobit)
 
-    }catch(eror){
-        console.log(eror)
-    }
-})
-app.post('/logs', async(req,res) =>{
-    const url = process.env.URL_LOG
-    const data = req.body
-    const options = {
-        method: 'POST',
-        url: url,
-        headers: {
-            authorization: 'Bearer 20250301efx', 
-            'content-type': 'application/json'
-        },
-        data: data
-    };
-      
-      try {
-        const { data } = await axios.request(options);
-        res.send(data)
-      } catch (error) {
-        res.send(error)
-      }
-
-    
-})
+app.post('/logs', postlogstobit)
